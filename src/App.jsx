f650cb7164de08cbfd8f6d2223f214e839ef68cc
CarlosPerez <carlosmperez2901@gmail.com>
2025-08-21 23:26:06 -0700

    Fix: App not waiting for the data to be fetched

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,62 @@
 import "./App.css";
 import NavBar from "./components/NavBar.jsx";
 import Card from "./components/Card.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const BLEACH_ID = 12;
 
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [visitedCardIds, setVisitedCardIds] = useState([]);
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    let ignore = false;
+
+    const url = `https://api.jikan.moe/v4/manga/${BLEACH_ID}/characters`;
+    const characterNames = [
+      "Abarai, Renji",
+      "Kuchiki, Rukia",
+      "Kurosaki, Ichigo",
+      "Aizen, Sousuke",
+      "Zaraki, Kenpachi",
+      "Urahara, Kisuke",
+    ];
+
+    if (!ignore) {
+      fetch(url, { mode: "cors" })
+        .then((response) => response.json())
+        .then((json) => json.data)
+        .then((data) =>
+          data.filter((character) =>
+            characterNames.includes(character.character.name),
+          ),
+        )
+        .then((filteredData) => {
+          setCharacters(
+            filteredData.map((character) => {
+              return {
+                id: character.character.mal_id,
+                name: character.character.name,
+                imgUrls: {
+                  jpg: character.character.images.jpg.image_url,
+                  webp: character.character.images.webp.image_url,
+                },
+              };
+            }),
+          );
+        })
+        .catch((error) => console.error("Err:", error));
+
+      console.log("FROM EFFECT: ");
+    }
+    return () => {
+      console.log("Disconnecting effect");
+      ignore = true;
+    };
+  }, []);
 
   function handleClick(e) {
     const card = e.target.type === "submit" ? e.target : e.target.parentNode;
@@ -20,16 +70,19 @@ function App() {
       setVisitedCardIds([...visitedCardIds, card.id]);
     }
   }
-
   return (
     <>
       <NavBar score={score} bestScore={bestScore}></NavBar>
       <main className="cards-container">
-        <Card id={0} onClick={handleClick}></Card>
-        <Card id={1} onClick={handleClick}></Card>
-        <Card id={2} onClick={handleClick}></Card>
-        <Card id={3} onClick={handleClick}></Card>
-        <Card id={4} onClick={handleClick}></Card>
+        {characters.map((character) => {
+          return (
+            <Card
+              id={character.id}
+              imgUrl={character.imgUrls.webp}
+              onClick={handleClick}
+            ></Card>
+          );
+        })}
       </main>
     </>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
 import "../styles/card.css";
 import reactSvg from "../assets/react.svg";
 
-export default function Card({ id, onClick }) {
-  const imgUrl = reactSvg;
+export default function Card({ id, imgUrl, onClick }) {
+  // const imgUrl = reactSvg;
   return (
     <button id={id} className="card" onClick={onClick}>
       <img src={imgUrl} />
